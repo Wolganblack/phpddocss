abecbdb352472436823b43f864ef470bd418c479
Wolganblack <ALFONSO@DESKTOP-7VNR7GU>
2022-05-24 19:30:55 -0600

    terminar

--- Formulario 4.0/recibe.php
+++ Formulario 4.0/recibe.php
@@ -12,11 +12,11 @@ $Salariofinal=$Salario+$bono;
 echo"<h3>Sistema de Cálculo</h3>";
 
 echo "<b> <i> El trabajador :$nombre Con el apellido: $apellido </b> </i>";
-echo "<b> <i> Su bono :$bono y su salario final es de: $Salariofinal </b> </i>";";
+echo "<b> <i> Su bono :$bono y su salario final es de: $Salariofinal </b></i>";
 }
 
- elseif(isset($_POST['nombre']) && (isset($_POST['Salario']) && $_POST['Salario']==1500 ) 
-&& isset($_POST['apellido'])) 
+elseif(isset($_POST['nombre']) && (isset($_POST['Salario']) && $_POST['Salario']==1500 ) 
+&& isset($_POST['apellido']))  
 {
 $nombre = $_POST['nombre'];
 $apellido= $_POST['apellido'];
@@ -27,7 +27,7 @@ $Salariofinal=$Salario+$bono;
 echo"<h3>Sistema de Cálculo</h3>";
 
 echo "<b> <i> El trabajador :$nombre Con el apellido: $apellido </b> </i>";
-echo "<b> <i> Su bono :$bono y su salario final es de: $Salariofinal </b> </i>";;
+echo "<b> <i> Su bono :$bono y su salario final es de: $Salariofinal </b> </i>";
 }
 
 elseif (isset($_POST['nombre']) && (isset($_POST['Salario']) && $_POST['Salario']==1800 ) 
